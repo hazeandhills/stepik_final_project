59263ccba268e53595e03242b93010ae3ddf4cad
hazeandhiils <samsonovas001@gmail.com>
2020-08-17 16:23:22 +0500

    add login mark

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,11 @@
 ﻿from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+import pytest
 
 # link = "http://selenium1py.pythonanywhere.com/"
 
 
+@pytest.mark.login
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
